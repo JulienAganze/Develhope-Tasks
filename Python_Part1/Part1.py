03f145e7592e4271e8517acf2d0218bd053e9ec0
JulienAganze <aganzejulien2017@gmail.com>
2022-12-16 18:58:17 +0100

    Additional changes

--- Python_Part1/Part1.py
+++ Python_Part1/Part1.py
@@ -1,6 +1,82 @@
-#Exercise Variables 1
-
+#%%Variables Exercise1
 firstName='Mario'
 
-#Exercise Variables 2
+#%%Variables Exercise2
+age=25
+
+#%%Variables Exercise3
+sentence="Hello, I'm Mario!" 
+
+#%%Variables Exercise4
+amount=2.14
+
+#Variables Exercise5
+
+
+
+#Variables Exercise6
+
+#Variables Exercise7
+
+
+#Variables Exercise8
+
+
+#Variables Exercise9
+
+
+#Variables Exercise10
+
+#%%Variables Exercise11 I should come back to this 
+a = 'hello' #capitalize
+a=a.capitalize()
+b = 'tom' #uppercase
+b=b.upper()
+c = 'LAURA' #lowercase
+c=c.lower()
+question = 'How are you' #change o in e
+question=question.replace('o','e')
+age_question = 'How old are you?' #use the correct method to create a string for each word
+age_question=age_question.split(' ')
+print(a, b, c, question, age_question)
+
+#Variables Exercise12
+
+
+
+#%%Methods Exercise1 comme back to this too
+print(type("Hello World"))
+print(type(True))
+print(type(False))
+print(type(33))
+print(type(24.5))
+print(type(4+1j))
+print(type(4j))
+print(type(["lion", "monkey", "dog","fish"]))
+print(type(("lion", "monkey", "dog","fish")))
+print(type({"name" : "John", "surname" : "Doe", "age":22}))
+print(type({"lion", "monkey", "dog","fish"}))
+
+#%%Nethods Exercise2
+num1 = 1.3
+num2 = 2.3
+num3 = 1j 
+num4 = 1.4 
+num5 = 1.5
+
+num1=float(num1)
+num2=int(num2)
+num3=complex(num3)
+num4=round(num4)
+num5=round(num5)
+print(num1,num2,num3,num4,num5)
+print(type(num1),type(num2),type(num3),type(num4),type(num5))
+#%%Methods Exercise3
+num1 = 1122334455666
+
+num1_str=str(num1)
+len(num1_str)
+num1_str[2]
+num1_str[2:5]
+
 
