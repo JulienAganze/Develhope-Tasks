01c8258a55dd3ba91c2c463e6b2c30e43177430b
JulienAganze <aganzejulien2017@gmail.com>
2023-01-24 10:19:56 +0100

    first 7 exercises

---
+++ Python_Part1/Part2.py
@@ -0,0 +1,126 @@
+#%% Iterators While loop
+i = 0
+while i < 3:
+    print('*', end = '')
+    i += 1 
+#%% Iterators For loop EXERCISE 1
+for i in range(0,6):
+    if i % 2 != 0:
+        print('*'*i)
+#%% Iterators For loop EXERCISE 2
+todo = ["exercise1", "exercise2", "exercise3","coffee break" ,"exercise4","exercise5","exercise6"]
+for x in todo:
+    if x .upper() == "COFFEE BREAK":
+        print(x)
+        break
+#%% Iterators For loop EXERCISE 3
+list1 = ["lion", "monkey", "dog","fish"]
+tuple1 = ("lion", "monkey", "dog","fish")
+set1 = {"lion", "monkey", "dog","fish"}
+dict1 = {"lion":"land", "monkey":"land", "dog":"land","fish":"water"}
+# Iterating through list
+for i in list1:
+    print(i)
+    
+# Iterating through turple
+for i in tuple1:
+    print(i)
+    
+# Iterating through set   
+for i in set1:
+    print(i)
+    
+# Iterating through dict    
+for x,y in dict1.items():
+    if y == 'land':
+        print(f'{x} lives in {y}')
+#%%  DATA STRUCTURES
+list1 = ["lion", "monkey", "dog","fish"]
+tuple1 = ("lion", "monkey", "dog","fish")
+set1 = {"lion", "monkey", "dog","fish"}
+dict1 = {"lion":4, "monkey":2, "dog":4,"fish":2}
+            # 1.PRINTING THE LENGTHS OF ALL ELEMENTS
+#length of list
+print(len(list1))
+#length of tuple
+print(len(tuple1))
+#length of set
+print(len(set1))
+#length of dict
+print(len(dict1))
+             # 2.PRINTING THE FIRST ELEMENT OF list1 and tuple1
+
+print(list1[0])
+print(tuple1[0])
+            # 3.PRINTING THE VALUE OF lion key of dict1
+print(dict1['lion'])
+            # 4.CHANGING THE SECOND POSITION OF list1 TO 'rabbit'
+list1[1] = 'rabbit'
+print(list1)
+            # 5.TRYING TO CHANGE THE 2nd position element of the tuple to 'rabbit'
+tuple1[1] = 'rabbit'
+#Trying to run thie above command i get an error because tuples are unchangeable or immmutable
+
+            # 6.ADING 'momkey' to list
+list1.append('monkey')
+print(list1)
+            # 7.REMOVUNG 'rabbit form the list
+list1.remove('rabbit')
+print(list1)
+            # 8. Fixing the wrong value of the number of feets for the fish
+dict1['fish'] = 0
+print(dict1)
+
+#%% EXERCISE 1 FUNCTIONS
+def goodbye():
+    print('good bye')
+goodbye()
+
+#%% EXERCISE 2 FUNCTIONS
+def goodbye(name):
+    print(f'Good bye {name}')
+goodbye('Julien')
+
+#%% EXERCISE 3 FUNCTIONS
+import os
+user = os.getlogin()
+print(user)
+
+
+#%% EXERCISE 4 FUNCTIONS
+def greeting():
+    family = ['John Doe','Tristram Mcbride','Baldwin Preston', 'Wally Collins']
+    for person in family:
+        print(f'Hello {person}!')
+greeting()
+#%% EXERCISE 4 FUNCTIONS  --version without classes
+def greeting(newcomer,default = 'John Doe'):
+    family = []
+    family.append(default)
+    family.append(newcomer)
+    for person in family:
+        print(f'Hello {person}!')
+greeting('Julien Chambali')
+#%% EXERCISE 4 FUNCTIONS -- version with classes
+class greeting:
+    def __init__(self,newcomer,default ='John Doe'):
+        self.newcomer = newcomer
+        self.default = default
+    def sayhello(self):
+        family = []
+        family.append(self.default)
+        family.append(self.newcomer)
+        for person in family:
+            print(f'Hello {person}!')
+            
+newperson = greeting('Julien Chambali')
+newperson.sayhello()     
+
+
+#%% EXERCISE 5 FUNCTIONS
+
+
+#%% EXERCISE 6 FUNCTIONS
+
+
+#%% EXERCISE 7 FUNCTIONS
